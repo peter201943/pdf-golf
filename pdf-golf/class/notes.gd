1baf2987638bc55549e244ca3f46e6b6c3fa8e46
Peter Mangelsdorf <pjm349@drexel.edu>
2020-08-02 12:49:48 -0400

    Add initial files

---
+++ pdf-golf/class/notes.gd
@@ -0,0 +1,41 @@
+extends Node
+
+"""md
+
+---
+time:	2020-8-2T11:49:00
+author:	peter_man
+---
+
+# Notes
+
+## about
+- an attempt to record documentation within godot itself
+- please stop me if this seems like a terrible idea
+
+## what we need
+- terrain generation
+	- [Voxel Tools for Godot](https://github.com/Zylann/godot_voxel)
+		- requires use of module -> needs to be compiled in
+		- custom binaries are available
+- global illumination
+	- might be enabled by default in the voxel demos
+
+
+## misc
+- [Godot Recipe: 3D KinematicBody: Align with terrain](https://www.youtube.com/watch?v=7axJJYont6Y)
+- [Create a Game Interface From Scratch in Godot 3 (tutorial)](https://youtube.com/watch?v=y1E_y9AIqow)
+- [How to get root node of a Scene (not of whole tree) - Godot Engine ...](https://godotengine.org/qa/25126/how-to-get-root-node-of-a-scene-not-of-whole-tree)
+
+## disabling
+- [How do I disable a node?](https://www.reddit.com/r/godot/comments/4afrab/how_do_i_disable_a_node/)
+- [How to disable/enable a node? - Godot Engine - Q&A](https://godotengine.org/qa/49696/how-to-disable-enable-a-node)
+
+## saving
+- [Save a scene and its variables?](https://godotengine.org/qa/50573/save-a-scene-and-its-variables)
+- [How to load and change scenes - Godot Engine - Q&A](https://godotengine.org/qa/24773/how-to-load-and-change-scenes)
+
+## pausing
+- [Unable to pause individual nodes, only tree](https://github.com/godotengine/godot/issues/15993)
+"""
+
