926bda2c7e9fb42c9e64bb7efb4da5f4be85ff52
Peter Mangelsdorf <pjm349@drexel.edu>
2020-09-21 02:54:17 -0400

    Simplify ready, add playing and stopping

--- Client/music-player/music-player.gd
+++ Client/music-player/music-player.gd
@@ -19,7 +19,7 @@ Play a Shuffled Selection of Music in Game
 export var playing: bool = false setget play
 
 # Where we emit music from
-var player: AudioStreamPlayer
+onready var player: AudioStreamPlayer = get_node("AudioStreamPlayer")
 
 # Control Shuffling of music
 export var shuffled: bool = false setget shuffle
@@ -27,27 +27,19 @@ export var shuffled: bool = false setget shuffle
 # Where we load our music from
 export(String, DIR) var music_folder = "res://music/" setget load_folder
 
-# Where we currently are in the playlist
-export(int, 0, 255) var current_song: int = 0
+# Where we currently are in the playlist # NOTE max size of 255 is arbitrary
+export(int, 0, 255) var current_song: int = 0 setget change_song
 
 # What the queue of music currently is
 export(Array, Resource) var playlist: Array
+var playlist_size: int = 0
 
 
 func _ready():
 	"""
-	Get the Player, Tracks
-	Shuffle the Music
+	Reset Random (for shuffling)
 	"""
-	
-	# Get the Player
-	player = get_node("AudioStreamPlayer")
-	
-	# Get the Tracks (Load from Filesystem)
-	# load_folder(music_folder) # ATTN (do we need this step? This will have already been loaded, and we do not want to reset this)
-	
-	# Shuffle the Tracks
-	shuffled = true
+	randomize()
 
 
 func play(value: bool) -> void:
@@ -55,21 +47,19 @@ func play(value: bool) -> void:
 	Allow an Editor to test playback and behavior of music player
 	"""
 	playing = value
+	if playing:
+		player.play()
+	if not playing:
+		player.stop()
 
-	# if true # FIXME (incomplete)
-	# if false # FIXME (incomplete)
 
-
-func shuffle(value: bool) -> void:
+func shuffle(_value: bool) -> void:
 	"""
 	Allow an Editor to Re-Shuffle the Tracks
 	"""
-	
 	# Note, we do not ever set shuffle to true, since it has no state
-	pass
-	
-	# if true # FIXME (incomplete)
-	# if false # FIXME (incomplete)
+	# Shuffle the playlist
+	playlist.shuffle()
 	
 
 func load_folder(value: String) -> void:
@@ -80,22 +70,40 @@ func load_folder(value: String) -> void:
 	# DEBUG
 	print("load folder called")
 	
+	# Does NOT trigger Setter
 	music_folder = value
 	
 	# on get new folder location, refresh the playlist
 	playlist.clear()
-	#for music_file in load(music_folder):
-		#playlist.append(load(music_file))
 	
-	# var what_is_this = load_folder(music_folder) # ATTN: This will crash the game!!!
+	# reset the playlist size
+	playlist_size = playlist.size()
+
+
+func change_song(value: int) -> void:
+	"""
+	Primary measn of changing tracks is by incrementing the currently playing song index
+	This triggers a load of the next track
+	If at end of list, move back to front
+	Also handle out of range values
+	"""
 	
-	# reshuffle the playlist
-	# shuffled = true # ATTN (do we want to reshuffle each time? maybe this is better left manually controlled)
+	# Do not accept out of bound values
+	# If found, reset to first song
+	if value < 0 or value > playlist_size:
+		value = 0
+	
+	# Does NOT Trigger Setter
+	current_song = value
+
+	# Load the Song
+	player.stream = load(playlist[current_song])
+	player.stream.loop = false
 
 
 func _on_AudioStreamPlayer_finished():
 	"""
 	When player finishes, load the next song
-	If at end of list, move back to front
 	"""
-	pass # Replace with function body.
+	# Trigger Setter
+	self.current_song += 1
