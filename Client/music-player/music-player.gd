8debdd2f4beb167f3e5f1340c892b075993925a2
Peter Mangelsdorf <pjm349@drexel.edu>
2020-09-21 04:54:56 -0400

    server-import-songs, fix-in-game-load-order (ugly-as-f***), now even more dazed and confused

--- Client/music-player/music-player.gd
+++ Client/music-player/music-player.gd
@@ -8,17 +8,24 @@ extends Node
 ## About
 Play a Shuffled Selection of Music in Game
 
+## Notes
+- The inspector does not automatically refresh.
+	- You need to click on something else to update the info displayed
+- It seems one cannot just go straight from `tool` -> `game`
+	- the getters/setters get called, and someone needs to either record their requests
+	- or make some final serialization step to prevent weird loading
+
 ## Help
-- NOTE! The inspector does not automatically refresh. You need to click on something else to update the info displayed
 - https://godotengine.org/qa/6099/list-of-export-hints
 	- amazing resource, keep it!
 - https://godotengine.org/qa/570/how-to-play-audio-whilst-changing-scene
 - https://godotengine.org/qa/20556/how-to-play-music-across-multiple-scenes-2d
+- https://blog.woubuc.be/post/background-music-player-godot/
 """
 
 
 # Control starting/stopping of music
-export var playing: bool = false setget play
+export var playing: bool = true setget play
 
 # Where we emit music from
 var player: AudioStreamPlayer
@@ -37,23 +44,56 @@ export(int, 0, 255) var current_song: int = 0 setget change_song
 export(Array, Resource) var playlist: Array
 var playlist_size: int = 0
 
+# Are we In-Editor or In-Game (Matters to load order)
+var loaded: bool = false
+
+# List of Lists (Short, Pair of `Command` and `Argument` -- see if godot can handle raw pointers)
+var missed: Array = []
+
 
 func _ready():
 	"""
 	Reset Random (for shuffling)
 	"""
-	
+
+	print("\t\t_READY") # DELETEME DEBUG (identify in-game call-order)
+
 	# No Identical Shuffles
 	randomize()
 	
+	# We are now "loaded"
+	loaded = true
+	
 	# prevent unloaded player
 	_check_player()
+	
+	# replay missed commands IN REVERSE (most recent last)
+	# Since we cannot reference functions directly, a parser is used
+	for command_arg in missed:
+		var command = command_arg[0]
+		var arg = command_arg[1]
+		print("missed: " + str(command) + "(" + str(arg) + ")")
+		if command == "play": play(arg)
+		if command == "shuffle": shuffle(false)
+		if command == "load_folder": load_folder(arg)
+		if command == "change_song": change_song(arg)
+		if command == "_on_AudioStreamPlayer_finished": _on_AudioStreamPlayer_finished()
 
 
 func play(value: bool) -> void:
 	"""
 	Allow an Editor to test playback and behavior of music player
 	"""
+
+	print("\t\tPLAY") # DELETEME DEBUG (identify in-game call-order)
+
+	# Prevent Incorrect Load Order when in-game
+	if not Engine.is_editor_hint() and not loaded:
+		# do not let anyone do anything yet
+		# yes, this will (inevitably) cause problems
+		# maybe use a queue to record requests and fulfill them once ready?
+		missed += [["play",value]]
+		return
 	
 	# prevent unloaded player
 	_check_player()
@@ -71,6 +111,16 @@ func shuffle(_value: bool) -> void:
 	"""
 	Allow an Editor to Re-Shuffle the Tracks
 	"""
+
+	print("\t\tSHUFFLE") # DELETEME DEBUG (identify in-game call-order)
+
+	# Prevent Incorrect Load Order when in-game
+	if not Engine.is_editor_hint() and not loaded:
+		# do not let anyone do anything yet
+		# yes, this will (inevitably) cause problems
+		# maybe use a queue to record requests and fulfill them once ready?
+		missed += [["shuffle",""]]
+		return
 	
 	# Note, we do not ever set shuffle to true, since it has no state
 	
@@ -85,6 +135,16 @@ func load_folder(value: String) -> void:
 	"""
 	Allow anyone to load a different folder of music
 	"""
+
+	print("\t\tLOAD_FOLDER") # DELETEME DEBUG (identify in-game call-order)
+	
+	# Prevent Incorrect Load Order when in-game
+	if not Engine.is_editor_hint() and not loaded:
+		# do not let anyone do anything yet
+		# yes, this will (inevitably) cause problems
+		# maybe use a queue to record requests and fulfill them once ready?
+		missed += [["load_folder",value]]
+		return
 	
 	# prevent unloaded player
 	_check_player()
@@ -113,6 +173,16 @@ func change_song(value: int) -> void:
 	If at end of list, move back to front
 	Also handle out of range values
 	"""
+
+	print("\t\tCHANGE_SONG") # DELETEME DEBUG (identify in-game call-order)
+	
+	# Prevent Incorrect Load Order when in-game
+	if not Engine.is_editor_hint() and not loaded:
+		# do not let anyone do anything yet
+		# yes, this will (inevitably) cause problems
+		# maybe use a queue to record requests and fulfill them once ready?
+		missed += [["change_song",value]]
+		return
 	
 	# prevent unloaded player
 	_check_player()
@@ -126,7 +196,7 @@ func change_song(value: int) -> void:
 	current_song = value
 
 	# Show What's Next
-	print("Now Playing: " + str(playlist[current_song].resource_name) + "[" + str(current_song) + "]")
+	print("Now Playing: " + str(playlist[current_song].resource_name) + " [" + str(current_song) + "]")
 	
 	# Load the Song
 	player.stream = playlist[current_song]
@@ -140,6 +210,17 @@ func _on_AudioStreamPlayer_finished():
 	"""
 	When player finishes, load the next song
 	"""
+
+	print("\t\tON_PLAYER_FINISHED") # DELETEME DEBUG (identify in-game call-order)
+
+	# Prevent Incorrect Load Order when in-game
+	if not Engine.is_editor_hint() and not loaded:
+		# do not let anyone do anything yet
+		# yes, this will (inevitably) cause problems
+		# maybe use a queue to record requests and fulfill them once ready?
+		missed += [["_on_AudioStreamPlayer_finished",""]]
+		return
+
 	# prevent unloaded player
 	_check_player()
 	# Trigger the Setter
@@ -175,9 +256,16 @@ func _check_player() -> void:
 	"""
 	Check if the player is loaded
 	and handle it if it is not
+	- 2020-09-21: Now that we have identified the criticality of calling `_ready()`, do we even need this function anymore?
 	"""
+
+	print("\t\tCHECK PLAYER") # DELETEME DEBUG (identify in-game call-order)
+
 	if not player:
+		print("player is not yet loaded")
+		print("Path: " + player_path)
 		player = get_node(player_path)
+		print("Player: " + str(player))
 
 
 
