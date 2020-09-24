596550123ecabedcc1eec2b20d6084aea56616fd
Peter Mangelsdorf <pjm349@drexel.edu>
2020-09-24 12:45:36 -0400

    Add Score and Health Counters

--- Client/health/health.gd
+++ Client/health/health.gd
@@ -2,8 +2,71 @@ extends Node
 
 
 """
-Brief Description
-""" # FIXME (documentation missing)
+Keeps Count of Health
+Emits Helath-Related Signals
+
+Once Health is Drained below zero, health must be re-instantiated
+"""
+
+
+# when we gain health
+signal heal
+
+# when we lose health
+signal hurt
+
+# when we lose too much health
+signal die
+
+# when we first start-up
+signal respawn
+
+
+# Total Health we can Have
+export(int, 0, 200) var max_life
+
+# Current Health we have
+var life: int setget change_life
+
+# whether we can change health
+var alive: bool
+
+
+func _ready():
+	"""
+	Reset the variables
+	"""
+	life = max_life
+	alive = true
+	emit_signal("respawn")
+
+
+func change_life(value: int):
+	"""
+	Add or Remove Life
+	"""
+	
+	# Cannot do much when you're already dead
+	if not alive:
+		return
+	
+	if value < 0:
+		emit_signal("hurt")
+		
+	if value > 0:
+		emit_signal("heal")
+	
+	life += value
+	
+	# check if we died
+	if life <= 0:
+		alive = false
+		emit_signal("die")
+
+
+
+
+
 
 
 
--- Server/score/score.gd
+++ Server/score/score.gd
@@ -2,8 +2,70 @@ extends Node
 
 
 """
-Brief Description
-""" # FIXME (documentation missing)
+Keeps Count of Score
+Emits Helath-Related Signals
+
+Once Health is Drained below zero, health must be re-instantiated
+"""
+
+
+# when we score points
+signal scored
+
+# when we lose points
+signal lost
+
+# when we win the game
+signal won
+
+# when we awaken
+signal counting
+
+
+# Total Score we can Have
+export(int, 0, 200) var max_score
+
+# Current Health we have
+var score: int setget change_score
+
+var scoring: bool
+
+
+func _ready():
+	"""
+	Reset the Variables
+	"""
+	score = max_score
+	scoring = true
+	emit_signal("counting")
+
+
+func change_score(value: int):
+	"""
+	Add or Remove Score
+	"""
+	
+	# hard to score once you've already won
+	if not scoring:
+		return
+	
+	if value < 0:
+		emit_signal("lost")
+		
+	if value > 0:
+		emit_signal("scored")
+	
+	score += value
+	
+	# check if we won
+	if score >= max_score:
+		scoring = false
+		emit_signal("won")
+
+
+
+
+
 
 
 
