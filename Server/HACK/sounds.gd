e89cd88d818d68f188b2759e5d5fe70bd8f4aa78
Peter Mangelsdorf <pjm349@drexel.edu>
2020-09-24 13:43:39 -0400

    repeat all the client sounds stuff on server

--- Server/HACK/sounds.gd
+++ Server/HACK/sounds.gd
@@ -2,7 +2,14 @@ extends Node
 
 
 """
-""" # FIXME (docs missing)
+Plays sounds on certain events
+Holds sound data (see exported interface)
+
+FOR THE LOVE OF GOD REFACTOR THIS INTO SOMETHING BETTER!!!
+(SOOOOO much duplicated code, my god)
+
+help: https://godotengine.org/qa/8135/choosing-a-random-word-from-an-array
+"""
 
 
 # Where we play hurt from (AND DIED)
@@ -21,22 +28,108 @@ onready var score = get_node(score_player_path)
 export var loss_player_path: NodePath = "loss"
 onready var loss = get_node(loss_player_path)
 
-# What we play on incremental events
+
+"""
+What we play on incremental events
+"""
+
+# Hurt Event
 export(Array, Resource) var hurt_sounds:    Array
+onready var hurts: int = hurt_sounds.size() # for random picking
+
+# Heal Event
 export(Array, Resource) var heal_sounds:    Array
+onready var heals: int = heal_sounds.size() # for random picking
+
+# Score Event
 export(Array, Resource) var score_sounds:   Array
+onready var scores: int = score_sounds.size() # for random picking
+
+# Loss Event
 export(Array, Resource) var loss_sounds:    Array
+onready var losses: int = loss_sounds.size() # for random picking
 
-# What we play on big life events
+# Death Event
 export(Array, Resource) var death_sounds:   Array
+onready var deaths: int = death_sounds.size() # for random picking
+
+# Respawn Event
 export(Array, Resource) var respawn_sounds: Array
+onready var respawns: int = respawn_sounds.size() # for random picking
 
-# What we play on big score events
+# Win Event
 export(Array, Resource) var win_sounds:     Array
+onready var wins: int = win_sounds.size() # for random picking
+
+# Counting Event
 export(Array, Resource) var start_sounds:   Array
+onready var starts: int = start_sounds.size() # for random picking
 
 
 
 
 
+
+"""
+Health Related Sound Effect Events
+"""
+
+func _on_health_died():
+	if hurt.playing():
+		hurt.stop()
+	hurt.stream = death_sounds[randi() % deaths]
+	hurt.play()
+
+
+func _on_health_healed(amount):
+	if heal.playing():
+		heal.stop()
+	heal.stream = heal_sounds[randi() % heals]
+	heal.play()
+
+
+func _on_health_hurt(amount):
+	if hurt.playing():
+		hurt.stop()
+	hurt.stream = hurt_sounds[randi() % hurts]
+	hurt.play()
+
+
+func _on_health_respawned():
+	if heal.playing():
+		heal.stop()
+	heal.stream = respawn_sounds[randi() % respawns]
+	heal.play()
+
+
+"""
+Score Related Sound Effects
+"""
+
+func _on_score_counting():
+	if score.playing():
+		score.stop()
+	score.stream = start_sounds[randi() % starts]
+	score.play()
+
+
+func _on_score_lost(amount):
+	if loss.playing():
+		loss.stop()
+	loss.stream = loss_sounds[randi() % losses]
+	loss.play()
+
+
+func _on_score_scored(amount):
+	if score.playing():
+		score.stop()
+	score.stream = score_sounds[randi() % scores]
+	score.play()
+
+
+func _on_score_won():
+	if loss.playing():
+		loss.stop()
+	loss.stream = win_sounds[randi() % wins]
+	loss.play()
 
